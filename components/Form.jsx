a07915f88327724ecf048c19c59910d064817e4f
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-08-27 19:56:50 +0300

    fix post method

--- components/Form.jsx
+++ components/Form.jsx
@@ -65,6 +65,43 @@ const Form = ({ products }) => {
       pattern: "^[0-9]{2}-[0-9]{3}$", // XX-XXX format
     },
   ];
+  ///check is postal code is correct
+  const isPostalCodeValid = (value) => {
+    const regex = /^[0-9]{2}-[0-9]{3}$/;
+    return regex.test(value);
+  };
+
+  const isCityValid = (value) => {
+    const regex = /^[^0-9]+$/;
+    return regex.test(value);
+  };
+  const isFirstNameValid = (value) => {
+    const regex = /^[a-zA-Z0-9]{3,16}$/;
+    return regex.test(value);
+  };
+  const isLastNameValid = (value) => {
+    const regex = /^[a-zA-Z0-9]{5,16}$/;
+    return regex.test(value);
+  };
+  console.log(
+    "firstname",
+    isFirstNameValid(values.firstName),
+    "lastname",
+    isLastNameValid(values.lastName),
+    "city",
+    isCityValid(values.city),
+    "postalcode",
+    isPostalCodeValid(values.postalcode)
+  );
+  const isFormValid = () => {
+    return isFirstNameValid(values.firstName) &&
+      isLastNameValid(values.lastName) &&
+      isCityValid(values.city) &&
+      isPostalCodeValid(values.postalcode)
+      ? true
+      : false;
+  };
+  console.log(isFormValid());
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
@@ -129,6 +166,7 @@ const Form = ({ products }) => {
       ))}
       <div class="mt-5 text-center">
         <button
+          disabled={!isFormValid()}
           class="w-1/2 px-6 py-2 text-blue-200 bg-blue-600 hover:bg-blue-900 "
           onClick={handleSubmit}
         >
